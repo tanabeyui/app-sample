815173f520cfa6fdb658ef2c6079468a629b6bb0
Yui Tanabe <ytogt356@gmail.com>
2022-12-21 14:40:35 +0000

    [Add] 楽天API：gemの追加

---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,8 @@
+RakutenWebService.configure do |c|
+  # (必須) アプリケーションID
+  c.application_id = ENV['1092332897678295938']
+
+  # (任意) 楽天アフィリエイトID
+  c.affiliate_id = ENV['2e3d170e.83b4b1e7.2e3d170f.1241af38']
+
+end
